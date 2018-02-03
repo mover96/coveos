b7ea222c228d7b7c4231a29c1b71c3c46c068ea1
Mitchell <mover333@gmail.com>
2018-02-03 15:30:48 -0800

    add react-iframe with delcaration file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,9 @@
+/// <reference path="../../react-iframe.d.ts" />
+
 import * as React from 'react'
 
 // import { FlowChartComponent } from './FlowChartComponent'
+import Iframe from 'react-iframe'
 
 export class App extends React.Component<null, null> {
   public render() {
